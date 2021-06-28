3783a8bab4a782859920782c50b03de592f41eeb
DiogoM14 <diogomartins200214@gmail.com>
2021-06-28 14:18:37 +0100

    Home page

---
+++ src/components/ListDivider/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import { styles } from './styles'
+import { theme } from '../../global/styles/theme'
+
+export function ListDivider() {
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  )
+}
